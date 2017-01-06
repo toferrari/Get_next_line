c9731d76a87b5b87e0d2af1f5e38986456de038e
Tony FERRARI <tferrari@e2r3p15.42.fr>
2017-01-06 17:25:19 +0100

    a

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/06 15:53:09 by tferrari          #+#    #+#             */
+/*   Updated: 2017/01/06 16:09:40 by tferrari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+
+int		get_next_line(const int fd, char **line);
+
+#endif

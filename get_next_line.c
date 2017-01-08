ef32ca80fc5470944e373c9b0aec05b97197a9c6
Tony FERRARI <tferrari@e3r6p15.42.fr>
2017-01-08 14:30:31 +0100

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,53 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 15:50:45 by tferrari          #+#    #+#             */
-/*   Updated: 2017/01/06 16:08:09 by tferrari         ###   ########.fr       */
+/*   Updated: 2017/01/08 13:18:04 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int		get_next_line(const int fd, char **line)
 {
+	static char *rest;
+	int ret;
+	char buff[BUFF_SIZE];
+	int i;
 
+	i = 0;
+	if (fd == -1)
+		return (GNL_error);
+	ret = read(fd, buff, BUFF_SIZE);
+	ft_putendl("a");
+	if (ret == 0)
+		return (GNL_end);
+	if (!(*line = ft_memalloc(BUFF_SIZE + 1)))
+		return (0);
+	ft_putendl("b");
+	/*ft_putnbr(BUFF_SIZE);
+	ft_putchar('\n');*/
+	while (buff[i] != '\n' && buff[i])
+	{
+		ft_putchar(*line[i]);
+		ft_putchar('\n');
+		*line[i] = buff[i];
+		i++;
+	}
+	ft_putendl("c");
+
+	if (buff[i] == '\n')
+	{
+		ft_putendl("d");
+		if (!(rest = ft_memalloc(BUFF_SIZE - i + 1)))
+			return (0);
+		rest = ft_strcpy(rest, (const char *)buff + i);
+	}
+	ft_putendl("d");
+	if (rest[0])
+		ft_putendl(rest);
+	return (1);
 }
--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/06 15:53:09 by tferrari          #+#    #+#             */
-/*   Updated: 2017/01/08 13:18:05 by tferrari         ###   ########.fr       */
+/*   Created: 2017/01/08 13:14:57 by tferrari          #+#    #+#             */
+/*   Updated: 2017/01/08 14:18:40 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include <get_next_line.h>
+#include <libft.h>
 
-# define BUFF_SIZE 50
-# define GNL_error -1
-# define GNL_end 0
+int main(int argc, char ** argv)
+{
+	char *line = NULL;
+	int fd;
 
-int		get_next_line(const int fd, char **line);
-
-#endif
+	fd = open(argv[1], O_RDONLY);
+	if (get_next_line(fd, &line));
+		ft_putendl(line);
+	return (0);
+}
